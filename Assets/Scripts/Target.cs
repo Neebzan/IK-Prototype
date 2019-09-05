da864c92f4e7df03a17b91551d7116ad0a934643
Esben Dalsgard <esbenjd@gmail.com>
2019-09-05 23:39:50 +0200

    Added material for ball. Changed scene

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -6,14 +6,21 @@ public class Target : MonoBehaviour
 {
     public float speedMultiplier = 1.0f;
 
+    private Vector3 startingPosition;
+
     public float xScale = 1.0f;
     public float yScale = 1.0f;
 
+    private void Start()
+    {
+        startingPosition = transform.position;
+    }
+
     void Update()
     {
-        float xPos = speedMultiplier * (Mathf.PerlinNoise(Time.time * xScale, 0f) - .5f);
-        float yPos = speedMultiplier * (Mathf.PerlinNoise(Time.time * yScale, 0f));
-        float zPos = speedMultiplier * (Mathf.PerlinNoise(Time.time * xScale, Time.time * yScale) - .5f);
-        transform.position = new Vector3(xPos, yPos, zPos);
+        float xPos = speedMultiplier * (Mathf.PerlinNoise(Time.time * xScale + startingPosition.x, 0f) - .5f);
+        float yPos = speedMultiplier * (Mathf.PerlinNoise(Time.time * yScale + startingPosition.x, 0f));
+        float zPos = speedMultiplier * (Mathf.PerlinNoise(Time.time * xScale + startingPosition.x, Time.time * yScale + startingPosition.y) - .5f);
+        transform.position = new Vector3(xPos, yPos, zPos) + startingPosition;
     }
 }
